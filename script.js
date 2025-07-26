eb460493a5ddae730eba6b09e1598a30068b4181
karakRohan <karakrohan2005@gmail.com>
2025-07-26 20:21:46 +0530

    99th Changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-let sections= document.querySelectorAll('section');
-let navLinks= document.querySelectorAll('header nav ul li a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset =sec.offsetTop - 150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav ul li a[href*=' + id + ' ]').classList.add(active)
@@ -23,5 +23,5 @@ window.onscroll = () => {
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-    
+
 }
